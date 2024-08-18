7bb07acfd55cab95817f05d63185104172ef1a6a
achmad rusdiana <achmad.rusdi123@gmail.com>
2024-08-18 07:49:50 +0700

    final

--- hbd2.py
+++ hbd2.py
@@ -3,22 +3,13 @@ from streamlit.components.v1 import html
 from urllib.parse import quote_plus
 import time
 
-# # Definisikan fungsi untuk pembersihan cache
-# @st.experimental_memo
-# def get_cache_cleared():
-#     return True
-
-# # Panggil fungsi untuk memaksa pembersihan cache
-# get_cache_cleared()
-
 # Pengaturan halaman
 st.set_page_config(page_title="Ucapan Ulang Tahun", page_icon="🎉", layout="centered")
-
 
 # Menambahkan musik latar belakang
 audio_file = open("music.mp3", "rb")
 audio_bytes = audio_file.read()
-st.audio(audio_bytes, format='audio/mp3', start_time=25, autoplay=True)
+st.audio(audio_bytes, format='audio/mp3', loop=True, autoplay=True)
 
 # Judul ucapan ulang tahun
 st.markdown("<h1 style='text-align: center; color: blue;font-size: 50px;'>Selamat Ulang Tahun!</h1>", unsafe_allow_html=True)
@@ -41,7 +32,7 @@ html_balloons = """
     return `hsl(${Math.random() * 360}, 70%, 50%)`;
   }
 
-  for (let i = 0; i < 100; i++) {
+  for (let i = 0; i < 1000; i++) {
     setTimeout(createBalloon, i * 100);
   }
 </script>
@@ -64,51 +55,100 @@ html_balloons = """
 """
 html(html_balloons)
 
-
 # Menampilkan foto
-st.image("foto.png", caption="Happy Birthday!", use_column_width=True)
+st.image("foto.png", caption="Selamat ulang tahun untukmu, wanita yang paling aku sayang. tiada kata yang bisa aku ucapkan kecuali rasa syukur karena telah mengenal kamu", use_column_width=True)
 
 # Ucapan
-st.markdown("<h2 style='text-align: center;'>Semoga hari ini penuh dengan kebahagiaan dan cinta. Selamat Ulang Tahun!</h2>", unsafe_allow_html=True)
+st.markdown("<h2 style='text-align: center;'>Sekali lagi! Selamat ulang tahun sayangkuuu hihi 😍</h2>", unsafe_allow_html=True)
 
+# Mengelola state untuk setiap tombol
+if "clicked_1" not in st.session_state:
+    st.session_state.clicked_1 = False
 
+if "clicked_2" not in st.session_state:
+    st.session_state.clicked_2 = False
 
+if "clicked_3" not in st.session_state:
+    st.session_state.clicked_3 = False
 
-
-# Putar video pendek
-if st.button("Tekan ini untuk kejutan pertama!"):
+# Tombol pertama
+if st.button("Klik disini ya sayang ❤️") or st.session_state.clicked_1:
+    st.session_state.clicked_1 = True
     st.balloons()
-    st.video("video.mp4")
+    st.image("IMG_20240804_192700_116.jpg")
+    text1 = """
+    Selamat ulang tahun ya! 
 
-# Menambahkan footer
+    Aku bersyukur banget bisa ada di samping kamu sampai hari ini. Melihat kamu tumbuh dan berubah menjadi versi terbaik dari dirimu sendiri adalah hadiah terbesar buat aku. Kamu lebih dewasa, lebih bijaksana, dan lebih kuat dari sebelumnya, dan itu bikin aku semakin yakin sama kita. 
 
-# Harapan
-st.write("**Tuliskan harapanmu di bawah ini:**")
-user_wish = st.text_input("Harapan:")
+    Aku harap aku bisa terus menjadi alasan di balik senyum kamu, tempat kamu pulang di saat suka dan duka. Doaku, semoga kamu selalu diberi kesehatan, panjang umur, dan kebahagiaan di setiap langkah yang kamu ambil. Meskipun tahun ini kita masih berjuang bersama - sama, aku yakin di ulang tahun berikutnya, kita akan merayakannya sebagai pasangan yang halal. 
 
+    Terima kasih sudah menjadi yang terbaik buat aku, dan aku janji akan selalu ada buat kamu. 
 
-# Jika pengguna sudah mengisi harapan
-if user_wish:
-    # Encode harapan untuk URL
-    time.sleep(3)
-    encoded_wish = quote_plus(user_wish)
-
-    # Membuat link WhatsApp
-    whatsapp_url = f"https://wa.me/6285728239628?text={encoded_wish}"
-    
-    # Menampilkan harapan dan balasan otomatis
-    st.write(f"Harapanmu: {user_wish}")
-    st.write("Aku pun berdoa itu akan menjadi kenyataan 🙏")
-
-    # Menyisipkan HTML dan JavaScript untuk menunggu 5 detik dan membuka tab baru
-    js_code = f"""
-    <script>
-    window.onload = function() {{
-        var url = "{whatsapp_url}";
-        window.open(url, "_blank");
-    }}
-    </script>
+    Selamat ulang tahun, sayang! ❤️
     """
-    st.markdown(js_code, unsafe_allow_html=True)
+    text1_html = text1.replace("\n", "<br>")
+    st.markdown(f"<h2 style='text-align: justify; color: grey;font-size: 15px;'>{text1_html}</h2>", unsafe_allow_html=True)
 
-st.markdown("<h3 style='text-align: center;'>🎂 Have a great day! 🎂</h3>", unsafe_allow_html=True)
+    # Tombol kedua
+    if st.button("Klik Lagi yaa 😘 ") or st.session_state.clicked_2:
+        st.session_state.clicked_2 = True
+        st.image("IMG-20240623-WA0027.jpg")
+        text2 = """
+        Satu hal yang selalu aku syukuri adalah bisa mengenalmu di dunia ini.
+
+        Kamu wanita yang cantik, ceria, dan penuh semangat. Setiap hari bersamamu adalah anugerah yang tak pernah membuat aku bosan. Senyummu, tawamu, dan caramu membawa kebahagiaan dalam hidupku membuat aku semakin yakin setiap harinya. Walaupun terkadang rasa amarah turut menyertai tapi itu membuat aku semakin tersadar bahwa kalau kamu begitu menyayangi aku hihi.
+
+        Dari awal aku mengenalmu hingga sekarang, satu hal yang tak pernah berubah adalah keinginanku untuk menjadikan wanita cantik ini sebagai istri aku.
+
+        Aku harap kita bisa terus bersama, sampai pada akhirnya kita bisa menjalani hidup sebagai sepasang suami istri yang bahagia. ❤️
+        """
+        text2_html = text2.replace("\n", "<br>")
+        st.markdown(f"<h2 style='text-align: justify; color: grey;font-size: 15px;'>{text2_html}</h2>", unsafe_allow_html=True)
+        
+        # Tombol ketiga
+        if st.button("Klik lagi 🥳 ") or st.session_state.clicked_3:
+            st.session_state.clicked_3 = True
+            st.image("IMG-20240607-WA0002.jpg")
+            text3 = """
+            Rasanya senang sekali bisa melihat senyummu, sayang. Meski aku tahu aku belum selalu bisa menjadi pria yang setiap saat membuatmu bahagia, tapi percayalah, dari lubuk hati yang paling dalam, aku selalu berusaha sekuat mungkin untuk mewujudkannya.
+
+            Aku tahu aku bukan orang yang sempurna, tapi izinkan aku terus mencoba. Maafkan aku jika pernah membuatmu bersedih. Aku sungguh berharap, mulai hari ini dan seterusnya, tak ada lagi air mata yang jatuh karena kata-kata, perbuatan, atau sikapku. Hatiku ikut hancur setiap kali melihatmu menangis.
+
+            Sekarang, coba kamu pejamkan matamu, dan doakanlah semua yang kamu harapkan.
+
+            
+            """
+            text3_html = text3.replace("\n", "<br>")
+            st.markdown(f"<h2 style='text-align: justify; color: grey;font-size: 15px;'>{text3_html}</h2>", unsafe_allow_html=True)
+            time.sleep(5)
+            st.markdown(f"<h2 style='text-align: center; color: black;font-size: 20px;'>Aamiin ya rabbal alamin, sayangku. 🌹</h2>", unsafe_allow_html=True)
+            st.markdown("<h3 style='text-align: center;'>🎂 Happy 26 sayangku! 🎂</h3>", unsafe_allow_html=True)
+
+# # Menambahkan footer
+# st.write("**Tuliskan harapanmu di bawah ini:**")
+# user_wish = st.text_input("Harapan:")
+
+# # Jika pengguna sudah mengisi harapan
+# if user_wish:
+#     # Encode harapan untuk URL
+#     time.sleep(3)
+#     encoded_wish = quote_plus(user_wish)
+
+#     # Membuat link WhatsApp
+#     whatsapp_url = f"https://wa.me/62895616059497?text={encoded_wish}"
+    
+#     # Menampilkan harapan dan balasan otomatis
+#     st.write(f"Harapanmu: {user_wish}")
+#     st.write("Aku pun berdoa itu akan menjadi kenyataan 🙏")
+
+#     # Menambahkan tombol untuk membuka WhatsApp
+#     st.markdown(f"""
+#     <a href="{whatsapp_url}" target="_blank">
+#         <button style="font-size:20px; padding:10px 20px; background-color: #25D366; color: white; border: none; border-radius: 5px; cursor: pointer;">
+#             Kirim Harapan Lewat WhatsApp
+#         </button>
+#     </a>
+#     """, unsafe_allow_html=True)
+
+
